0c96dd718526ba88fbca236a059de007b9b587da
Adam Pod <adam.podoxin@gmail.com>
2019-05-18 23:31:25 -0700

    Create food generator

--- Assets/Scripts/GameController.cs
+++ Assets/Scripts/GameController.cs
@@ -8,10 +8,17 @@ public class GameController : MonoBehaviour
     public DayEvent startDay;
     public DayEvent stopDay;
 
+    public GameObject foodPrefab;
+
     private int creaturesEnded = 0;
+
+    [SerializeField]
+    private int currentFoodAmount = 20;
 
     private void Start()
     {
+        GenerateFood();
+
         startDay.Invoke();
     }
 
@@ -22,7 +29,17 @@ public class GameController : MonoBehaviour
 
         yield return new WaitForSeconds(1);
 
+        GenerateFood();
         startDay.Invoke();
+    }
+
+    private void GenerateFood()
+    {
+        int amountOfFoodInMap = GameObject.FindGameObjectsWithTag("Food").Length;
+        for (int i = 0; i < currentFoodAmount - amountOfFoodInMap; i++)
+        {
+            Instantiate(foodPrefab, new Vector3(Random.Range(-6f, 6f), 0.5f, Random.Range(-6f, 6f)), Quaternion.identity);
+        }
     }
 
     public void CreatureEnded()
